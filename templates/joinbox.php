0c44d256bc7b08480958922a66339dfd529d25b6
mike <mike@Cowobo>
2012-03-09 10:50:20 +0300

    Made some placeholder text

--- templates/joinbox.php
+++ templates/joinbox.php
@@ -1,7 +1,11 @@
 <div class="large" id="join">
 	<div class="holder">
 		<div class="content">
-			JOIN US! YOU FRIGGIN' BASTARD!
+            <p>Time to explain the rules of the game.</p>
+            <p>To add content to Cowobo, you first have to login or make an account.</p>
+            <p>Then you can comment. Still, if you really want to add content, you really do have to update your profile first.</p>
+			<p>So let's start cracking:</p>
+            <?php echo $social->cowobo_connect(); ?>
 		</div>
 		<div class="scrolltrack"><div class="slider"></div></div>
 	</div>
