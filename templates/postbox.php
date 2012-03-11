0903693b740397037549931e7fa557ef2de13c34
mike <mike@Cowobo>
2012-03-11 21:37:01 +0300

    Made special provisions for profile edits: new profile 'save' => 'create' & no delete for user profile.

--- templates/postbox.php
+++ templates/postbox.php
@@ -3,8 +3,8 @@
 		<div class="content"><?php
 		$postcat = cwob_get_category($post->ID);
 		$posttype = $postcat->slug;
-		global $author; global $social; unset($profileids); 
-		
+		global $author; global $social; unset($profileids);
+
 		//load related posts and check if user has a profile to edit post
 		$relatedposts = new Cowobo_Feed(array('posts' => $post->ID));
 		if($relatedposts = $relatedposts->get_related()):
@@ -17,33 +17,43 @@
 			foreach($profiles as $profile):
 				$profileids[] = $profile->ID;
 			endforeach;
-		else: 
+		else:
 			$profileids = array();
 		endif;
 		if(in_array($social->profile_id, $profileids) or $post->post_author == get_current_user_id()):
 	 		$author = true; else: $author = false;
 		endif;
-		
+
 		//load the templates
 		if(file_exists(TEMPLATEPATH.'/templates/'.$post->post_name.'.php')):
 			include(TEMPLATEPATH.'/templates/'.$post->post_name.'.php');
 		elseif ( file_exists(TEMPLATEPATH.'/templates/' . $posttype . '.php') ):
-			include(TEMPLATEPATH.'/templates/' . $posttype . '.php');	
+			include(TEMPLATEPATH.'/templates/' . $posttype . '.php');
 		else:
-			include(TEMPLATEPATH.'/templates/default.php');	
+			include(TEMPLATEPATH.'/templates/default.php');
 		endif;?>
-		
+
 		</div>
 		<div class="scrolltrack"><div class="slider"></div></div>
 	</div>
 	<div class="cowobo_social_share"></div>
 	<div class="arrow"><?php
 		if($author):?>
-			<span class="save button" id="save-<?php echo $post->ID;?>">Save</span>
-			<span class='cowobo_social_like button'>Share</span>
-			<span class="delete button">Delete</span><?php
-		else:
-			$prev = get_adjacent_post(true,'',false); 
+			<span class="save button" id="save-<?php echo $post->ID;?>">
+                <?php if ( $post->post_status == 'draft' ) : ?>
+                    Create
+                <?php else: ?>
+                    Save
+                <?php endif; ?>
+            </span>
+            <?php if ( $post->post_status == 'publish' ) : ?>
+                <span class='cowobo_social_like button'>Share</span>
+            <?php endif; ?>
+			<?php if ( $post->ID != $social->profile_id ) : ?>
+                <span class="delete button">Delete</span>
+            <?php endif; ?>
+        <?php else:
+			$prev = get_adjacent_post(true,'',false);
 			$next = get_adjacent_post(true,'',true);?>
 			<span class="<?php if(!empty($prev)) echo 'lastpost button';?>" id="last-<?php echo $prev->ID; ?>">Last</span>
 			<span class="cowobo_social_like button">Share</span>
