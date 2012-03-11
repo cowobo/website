6f30d311a57d64ff83fd6f6df291eede538337d1
mike <mike@Cowobo>
2012-03-11 17:57:25 +0300

    Added some tests for the profile lightbox

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 get_header();
-global $social; $state = $social->state; ?>
+global $social; $profile_in_feed = false; ?>
 
 <div id="scroller" style="<?php if(is_single()) echo 'display:none';?>"><?php
 	include(TEMPLATEPATH.'/templates/newthumb.php');
 	foreach($newposts as $post): setup_postdata($post); $wp_query->in_the_loop = true; $counter++;
+        // Check if the current user's profile is in the feed already
+        if ( $social->state == 4 && $post->ID == $social->profile_id ) $profile_in_feed = true;
 		$typepost = cwob_get_category($post->ID);
 		include(TEMPLATEPATH.'/templates/postthumb.php');
 	endforeach;?>
@@ -14,7 +16,7 @@ global $social; $state = $social->state; ?>
 <?php
 
 //Lightboxes
-switch ( $state ) :
+switch ( $social->state ) :
     case 1:
         /**
          * @todo edit joinbox
@@ -23,15 +25,18 @@ switch ( $state ) :
         break;
     case 4:
         include(TEMPLATEPATH.'/templates/newbox.php');
+        if ( $profile_in_feed ) break;
     case 2:
     case 3:
         /**
          * @todo something wrong here. postholder doesn't load profile post. perhaps due to draft status?
          */
+        console_log ( "Postid: $postid; Profileid: {$social->profile_id}");
         if ($postid != $social->profile_id ) :
             $post = get_post ( $social->profile_id );
             setup_postdata($post);
             $wp_query->in_the_loop = true;
+            console_log ( "Postid profile: {$post->ID}");
             include(TEMPLATEPATH.'/templates/postbox.php');
             break;
         endif;
