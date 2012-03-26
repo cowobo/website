3e7fc0da010c4012b76c8b7c174c752a7a0ff0d4
mike <mike@Cowobo>
2012-03-26 17:23:29 +0300

    Added documentation

--- lib/class-cowobo-feed.php
+++ lib/class-cowobo-feed.php
@@ -1,42 +1,36 @@
 <?php
-/* ============= Constructs the Cowobo Feed.============== *
-* 	Syntax:
-* 	<?php $posts = new Cowobo_Feed ( $args, $wp_query ); ?>
-*
-* 	Arguments:
-* 	$args = array(
-* 		'posts' => [ array or single post ],
-* 		'cats'=> [ array, comma separated or single category ] ,
-* 		'users' => [ array or single user id ] (fetches personal feed),
-* 		'sort' => [ ASC or DESC ]
-*	);
-*	$wp_query = [ Normal WordPress query ]
-*
-*	To get the feed sorted and arranged, use $posts->get_feed();
-*========================================================*/
+/**
+ * Constructs the Cowobo feed
+ *
+ * Syntax: <?php $posts = new Cowobo_Feed ( $args, $wp_query ); ?>
+ * To get the feed sorted and arranged, use $posts->get_feed();
+ *
+ * @param arr Arguments:  'posts' => [ array or single post ], 'cats'=> [ array, comma separated or single category ] , 'users' => [ array or single user id ] (fetches personal feed), 'sort' => [ ASC or DESC ]
+ * @param arr $wp_query = [ Normal WordPress query ]
+ */
 
 class Cowobo_Feed {
-	
-	function __construct($args, $wp_query = false) { 
+
+	public function __construct($args, $wp_query = false) {
 		//  Get all queried posts (array || single)
-		if ( is_array($args['posts']) ) { 
-			foreach($args['posts'] as $postid) { 
+		if ( is_array($args['posts']) ) {
+			foreach($args['posts'] as $postid) {
 				$this->get_post_feed($postid);
 			}
 		}
 		elseif (!empty($args['posts'])) $this->get_post_feed($args['posts']);
-		
+
 		//  Get all queried cats (array || comma separated || single)
-		if ( is_array($args['cats']) ) { 
-			foreach($args['cats'] as $catid) { 
+		if ( is_array($args['cats']) ) {
+			foreach($args['cats'] as $catid) {
 				$this->get_cat_feed($catid);
 			}
 		}
 		elseif (!empty($args['cats'])) $this->get_cat_feed($args['cats']);
-		
+
 		//  Get all queried personal feeds (array || single)
-		if ( is_array($args['users']) ) { 
-			foreach($args['users'] as $uid) { 
+		if ( is_array($args['users']) ) {
+			foreach($args['users'] as $uid) {
 				$this->get_user_feed($uid);
 			}
 		}
@@ -51,17 +45,21 @@ class Cowobo_Feed {
 
 		// If we get no query or got no results, return the wiki [temporary]
 		if ( empty($this->feedposts) ) $this->get_cat_feed(1);
-		
+
 		// Set the description and titles
 		$this->feed_title = "Cowobo Favourite Feed";
 		$this->feed_description = $this->get_titles();
-		
+
 		// Set sorting direction
 		if (empty($args['sort'])) $args['sort'] = 'DESC';
 		$this->direction = $args['sort'];
 	}
 
-	// Feed for users
+	/**
+     * Feed for users
+     * @param int $userid
+     * @return null|boolean
+     */
 	function get_user_feed( $userid ) {
 		$feed_query = get_user_meta($userid,'cowobo_feed',true);
 		if (!is_array($feed_query)) return null;
@@ -72,12 +70,17 @@ class Cowobo_Feed {
 		}
 		return true;
 	}
-	
-	// Feed for posts
+
+    /**
+     * Feed for posts
+     *
+     * @param int $postid
+     * @return boolean
+     */
 	function get_post_feed( $postid ) {
 		// Get the array of related posts
 		$newposts =  Cowobo_Related_Posts::cwob_get_related_posts($postid);
-		
+
 		// If there are no related posts, only return this one post
 		if (empty($newposts)) $newposts = array(get_post($postid));
 		else $this->related_posts = $newposts;
@@ -90,8 +93,13 @@ class Cowobo_Feed {
 		$this->feed_titles[] = get_the_title($postid);
 		return true;
 	}
-	
-	// Feed for categories
+
+    /**
+     * Feed for categories
+     *
+     * @param obj $cats
+     * @return boolean
+     */
 	function get_cat_feed($cats) {
 		$args = array(
 			'category'        => $cats,
@@ -100,12 +108,12 @@ class Cowobo_Feed {
 			'order'             => 'ASC',
 			'post_type'      => 'post'
 		);
-		
+
 		// Get posts and merge
 		if (empty($this->feedposts))
 			$this->feedposts = get_posts($args);
 		else $this->feedposts = array_merge(get_posts($args), $this->feedposts);
-		
+
 		// Get the titles
 		$cat = explode(',',$cats);
 		foreach ( $cat as $c ) {
@@ -114,13 +122,24 @@ class Cowobo_Feed {
 		return true;
 	}
 
-	// Convert PHP timestamp to RSS
+    /**
+     * Convert PHP timestamp to RSS
+     *
+     * @param int $timestamp
+     */
 	function rss_date( $timestamp = null ) {
 	  $timestamp = ($timestamp==null) ? time() : $timestamp;
 	  echo date(DATE_RSS, $timestamp);
 	}
 
-	// Handle the limit on text length
+	/**
+     * Handle the limit on text length
+     *
+     * @param str $string
+     * @param int $length
+     * @param str (optional) $replacer
+     * @return str
+     */
 	function rss_text_limit($string, $length, $replacer = '...') {
 	  $string = strip_tags($string);
 	  if(strlen($string) > $length)
@@ -128,7 +147,10 @@ class Cowobo_Feed {
 	  return $string;
 	}
 
-	// Add image where available
+	/**
+     * Add image where available
+     * @param int $postid
+     */
 	function add_image($postid) {
 		$img = cwob_get_first_image($postid);
 		if($img) {
@@ -136,28 +158,40 @@ class Cowobo_Feed {
 			echo $image;
 		}
 	}
-	
-	// Return the feed sorted and cleaned of doubles
+
+	/**
+     * Return the feed sorted and cleaned of doubles
+     *
+     * @return arr
+     */
 	function get_feed() {
 		$postlist = remove_doubles($this->feedposts);
 		return array_object_sort( $postlist, 'post_date_gmt', $this->direction );
 	}
-	
-	// Returns related (default = unsorted)
+
+	/**
+     * Returns related (default = unsorted)
+     * @param bool (optional) $sort
+     * @return arr related posts
+     */
 	function get_related($sort = false) {
 		if ( !$this->related_posts ) return false;
 		if ($sort) {
 			$postlist = remove_doubles($this->related_posts);
-			return array_object_sort( $postlist, 'post_date_gmt', $this->direction );		
+			return array_object_sort( $postlist, 'post_date_gmt', $this->direction );
 		}
 		else return $this->related_posts;
 	}
-	
-	// Gives the position of the last post in the array
+
+	/**
+     * Gives the position of the last post in the array
+     *
+     * @return int
+     */
 	function last_post() {
 		return count($this->feedposts) - 1;
 	}
-	
+
 	// Make pretty sentence of all the titles.
 	function get_titles() {
 		$feed_title = '';
