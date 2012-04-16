444605631139b6b51343b80ce235c3f8ae3ad84c
Balthazar <bsieders@gmail.com>
2012-04-16 11:47:15 +0200

    Improved RSS box template

--- templates/rssbox.php
+++ templates/rssbox.php
@@ -2,16 +2,20 @@
 	<div class="holder">
 		<div class="content">
             <div class="gallery<?php if($author):?> editable<?php endif;?>">
-			<div class="topshadow"><h1>Stay Connected  via RSS</h1></div>
+			<div class="topshadow"><h1><?php the_title();?></h1></div><?php
+			foreach(get_children(array('post_parent' => $post->ID, 'numberposts' => -1, 'post_mime_type' =>'image', 'order'=>'ASC')) as $image):?>
+			<div class="slide"><?php
+				$imgsrc = wp_get_attachment_image_src($image->ID, $size = 'large');?>
+				<img src="<?php echo $imgsrc[0];?>" width="100%" alt=""/>
+ 			</div><?php
+			endforeach;?>
 			</div>
 			<?php if (!is_home): ?>
                 <h3>Get the feed for "<?php echo cowobo_get_pagetitle() ?>"</h3>
             <?php else: ?>
                 <h3>Get the feed for "Coders Without Borders"</h3>			
 			<?php endif; ?>
-            <ul>
-                <?php $social->print_rss_links(false, '<li>', '</li>'); ?>
-            </ul>
+			<div class="rssicons"><?php $social->print_rss_links(false); ?></div>
 		</div>
 		<div class="scrolltrack"><div class="slider"></div></div>
 	</div>
